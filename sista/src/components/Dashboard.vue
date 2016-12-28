35c01a62f214148e6000344c1d60c4a6afbc6d30
César Berdejo <mrkiller4@hotmail.com>
2016-12-28 10:20:37 -0600

    dashboard correcto añadido

--- sista/src/components/Dashboard.vue
+++ sista/src/components/Dashboard.vue
@@ -38,49 +38,29 @@
             </div>
 <!-- SERVICIOS -->
             <div class="form-group">             
-            	<select class="form-control" v-model="tipo_id" name="tipo_id">
-  					<option>Elija un servicio</option>
-  					<option value="00:30:00" id="1">Limpieza dental</option>
-  					<option value="01:00:00" id="2">Extracciones</option>
-  					<option value="00:45:00" id="3">Endodoncia</option>
-  					<option value="00:35:00" id="4">Cita médica</option>
+            <label for="message-text" class="form-control-label">Escoja un servicio</label>
+            	<select class="form-control" v-model="tipo_id">
+            	<option v-for="option in options" :value="option.id">
+            		{{ option.text }}
+            	</option>
+  					
 				</select>
             </div>
 <!-- Hora -->
             <div class="form-group">
               <label for="message-text" class="form-control-label">Duración:</label>
 				<select class="form-control" v-model="fecha_inicio" name="fecha_inicio">
-  					<option id="0">00:00</option>
-  					<option id="1">01:00</option>
-  					<option id="2">02:00</option>
-  					<option id="3">03:00</option>
-  					<option id="4">04:00</option>
-  					<option id="5">05:00</option>
-  					<option id="6">06:00</option>
-  					<option id="7">07:00</option>
-  					<option id="8">08:00</option>
-  					<option id="9">09:00</option>
-  					<option id="10">10:00</option>
-  					<option id="11">11:00</option>
-  					<option id="12">12:00</option>
-  					<option id="13">13:00</option>
-  					<option id="14">14:00</option>
-  					<option id="15">15:00</option>
-  					<option id="16">16:00</option>
-  					<option id="17">17:00</option>
-  					<option id="18">18:00</option>
-  					<option id="19">19:00</option>
-  					<option id="20">20:00</option>
-  					<option id="21">21:00</option>
-  					<option id="22">22:00</option>  				
-  					<option id="23">23:00</option>
+				<option v-for="hour in hours" :value="hour.value">
+					{{ hour.text }}
+				</option>
+  					
 				</select>
             </div>            
           </form>
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-          <button type="button" class="btn btn-primary">Agendar Cita</button>
+          <button type="button" class="btn btn-primary" v-on:click="sendCalendarJSON">Agendar Cita</button>
         </div>
       </div>
 </div>
@@ -93,23 +73,40 @@
 <script>
 var id_count = 0;
     export default {
-    	datos: function(){
-    		return{
-    			id_cita: '',
-    			calendario_id: '',
+    	name:'dashboard',
+
+    	data: function(){
+    		return {
+    			//id_cita: '',
+    			calendario_id: 2,
     			tipo_id: '',
     			fecha_inicio: '',
     			fecha_final: '',
     			cliente_nombre: '',
     			cliente_telefono: '',
-    			cliente_email: ''
-    	}
-    },
-
-    	name:'dashboard',
-    	data(){
-    		return {
-    			citas : [],
+    			cliente_email: '',
+    			hora_cita: '',
+    			date_selected : '',
+    			options:[
+    				{ text: 'Limpieza dental', duration: 30, id: 0},    		
+    				{ text: 'Extracciones', duration: 60, id: 1}
+    				//{ text: 'Endodoncia', duration: 80, id: 2},
+	    			//{ text: 'Cita medica', duration: 120, id: 3}
+    			],
+    			hours:[    			
+    				{ text: '00:00', value: '2016-12-31 00:00:00', id: 0},
+    				{ text: '01:00', value: '2016-12-31 01:00:00', id: 1},
+    				{ text: '02:00', value: '2016-12-31 02:00:00', id: 2},
+    				{ text: '03:00', value: '2016-12-31 03:00:00', id: 3},
+    				{ text: '04:00', value: '2016-12-31 04:00:00', id: 4},
+    				{ text: '05:00', value: '2016-12-31 05:00:00', id: 5},
+    				{ text: '06:00', value: '2016-12-31 06:00:00', id: 6},
+    				{ text: '07:00', value: '2016-12-31 07:00:00', id: 7},
+    				{ text: '08:00', value: '2016-12-31 08:00:00', id: 8},
+    				{ text: '09:00', value: '2016-12-31 09:00:00', id: 9},
+    				{ text: '10:00', value: '2016-12-31 10:00:00', id: 10},
+    		
+    			]
     		}
     	},
     	mounted(){
@@ -119,40 +116,104 @@ var id_count = 0;
     	methods : {
 
     		sendCalendarJSON: function(){
+    				//guardo en id_selected el valor seleccionado en el option 
+    				var id_selected = this.tipo_id;
+    				//console.log(this.tipo_id);
+    				//mediante la funcion $.greep de JQuery se hace una busqueda en el arreglo de objetos, el parametro de busqueda es id 
+    				// ejemplo:  { text: 'Limpieza dental', duration: '00:30:00', id: '0'} devolverà este objeto del arreglo si el id que le pasas en el return es 0 
+    				var service = $.grep(this.options, function(e){ return e.id == id_selected; });
+    				// si la cantidad de resultados que te devuelve $.greep es cero quiere decir que no existieron resultados
+    				if (service.length == 0) {
+    					//si no se encontraron resultados avisale al usuario ... y abortas el mètodo con un return; ò return false; 
+ 					 // not found
+					} else if (service.length == 1) {
+				  // access the foo property using result[0].foo
+				  // $.greep siempre te va a devolver un array de los resultados obtenidos, obviamente si solo quieres un resultado lo encontraràs en la posiciòn 0 
+				  var servicio = service[0];
+				 
+				  // entonces ese resultado se guardò en servicio 
+
+
+				} else {
+				  // multiple items found
+				}
+
+				//console.log(servicio);
+				//return;
+
+
     			this.errorMessage='';
+    			var dia_seleccionado = this.date_selected;
+
+    			var dateformat = new Date(this.fecha_inicio);       			
+    			var fecha_final = this.addMinutes(dateformat,servicio.duration);
+    			var month = fecha_final.getMonth() < 10 ? '0'+fecha_final.getMonth() : fecha_final.getMonth();
+    			var day = fecha_final.getDay() < 10 ? '0'+fecha_final.getDay() : fecha_final.getDay();
+    			var year = fecha_final.getFullYear();
+    			var minute = fecha_final.getMinutes() < 10 ? '0'+fecha_final.getMinutes() : fecha_final.getMinutes();
+    			var hora = fecha_final.getHours() < 10 ? '0'+fecha_final.getHours() : fecha_final.getHours();
+    			var segundo = fecha_final.getSeconds() < 10 ? '0'+fecha_final.getSeconds() : fecha_final.getSeconds();
+
+    			fecha_final = year + "-" + month + "-" + day +" " + hora +":"+ minute +":"+ segundo;
+    			console.log(fecha_final);
+    			//var parse_endDate = 
+    			//console.log(fecha_final.toISOString());
+    		//	console.log(fecha_final.toString());
+
+
     			var calendarInformation = {
-    				"id_cita": id_count,
+    				//"id_cita": id_count,
     				"calendario_id": this.calendario_id,
-    				"tipo_id": this.tipo_id,
-    				"fecha_inicio": this.fecha_inicio+$('#calendar').fullCalendar('getDate'),
-    				"fecha_final": '',
+    				"tipo_id": servicio.id,
+    				"fecha_inicio": this.fecha_inicio,
+    				//valueHoraInicio + valueHoraServicio
+    				"fecha_final": fecha_final,
     				"cliente_nombre": this.cliente_nombre,
     				"cliente_telefono": this.cliente_telefono,
     				"cliente_email": this.cliente_email
-    			}
+    			};    			
+    			var objThis = this;
+    			this.$http.post('cita?token='+localStorage.getItem('token'), calendarInformation).then(
+						//success
+						function(response){
+							console.log("Success");
+						},
+						//error
+						function(response){							
+							
+							if(response.status == 401){
+								this.errorMessage = "Error";
+							}
+							else{
+								//console.log(response.data);
+							}
+							//console.log(response.data);
+						});
     		},
+    		addMinutes : function(date,minutes){
 
+    			return new Date(date.getTime() + minutes*60000);
+
+    		},
             createCalendar : function(){
+            	var thisObj = this;
             $('#calendar').fullCalendar({
 
-            dayClick:  function(event, jsEvent, view) {
+            dayClick:  function(date, jsEvent, view){
+            	thisObj = date;
+            	//console.log(thisObj);
+
             	//$('#modalTitle').html(event.title);
             	//$('#modalBody').html(event.description);
             	//$('#eventUrl').attr('href',event.url);
             	$('#calendarModal').modal('show');
         },
-/* INICIO ACTUALIZAR AL HACER CLICK
-            	eventClick: function(event, element){
-            		event.title = "clicked";
-            		$('#calendar').fullCalendar('updateEvent', event);
-            	},
-FIN FUNCION ACTUALIZAR AL HACER CLICK*/
             header: {
                 left: 'prev,next today',
                 center: 'title',
                 right: 'month,agendaWeek,agendaDay'
             },
-            defaultDate: '2016-12-12',
+            
             navLinks: true, // can click day/week names to navigate views
             selectable: true,
             selectHelper: true,
@@ -179,7 +240,7 @@ FIN FUNCION ACTUALIZAR AL HACER CLICK*/
         })
 
             },
-
+            timeFormat: 'H(:mm)',
     		fetchDatas : function(){
     		
                 var thisObj = this;
